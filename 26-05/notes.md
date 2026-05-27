a451d2f20d5916d3aa97ac9523ffaea1dc33ee1f
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-05-27 23:26:00 +0530

    may 27

---
+++ 26-05/notes.md
@@ -0,0 +1,1 @@
+prompt hme string deta h
--- 26-05/script.js
+++ 26-05/script.js
@@ -1,1 +1,26 @@
 // day 33
+
+// for(let i=1;i<=20;i++)
+// {
+//   if(i%2==0)
+//   {
+//       console.log(i)
+//   }
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Yes");
+// }
+
+// let n = +prompt("Give number ?");
+
+// if (n > 0) console.log("Positive");
+// else if (n < 0) console.log("Negative");
+// else console.log("Zero");
+
+
+// day 35
