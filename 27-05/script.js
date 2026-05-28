6db0bceb3c63acfcb640def3d5c7eb03d8caed43
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-05-28 23:42:31 +0530

    may 28

---
+++ 27-05/script.js
@@ -0,0 +1,56 @@
+// lecture - 37
+
+// (scoping) =>
+//   IIFE  for making private variable
+
+// (function () {
+//     console.log("Arun");
+//   })();
+
+//arrow function
+
+// let fn = () => {
+//   console.log("Arun kumar");
+// }
+
+// fn();
+
+// anonymous function
+
+// function ()
+// {
+
+// }
+
+// hof => ek aisa function jo return kre ek or function ya to bo fn accept kr le parameter m
+// function abcd()
+// {
+//   return function(){
+
+//   }
+// }
+
+//call back 
+// function abcd(a)
+// {
+
+// }
+
+// abcd(function(){
+//   // this is callback function...
+// })
+
+// first class function => ek concept h function us lng m fn variable ki trh use ho skte h
+// function abcd(a,b){
+
+// }
+// abcd(function (){
+
+// },2);
+
+// day 38
+
+
+
+
+
