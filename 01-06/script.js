40310ae8dd713c6b21a9d09182280f6ee433446d
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-06-01 17:06:27 +0530

    june 1

---
+++ 01-06/script.js
@@ -0,0 +1,73 @@
+// DOM : frontend ki javascript
+
+// DOM 4 pillars
+// 1. selection of an element ✔
+// 2. changing html ✔
+// 3. changing CSS
+// 4. eventlistener
+
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "Hello world!!!";
+// h1.style.color = "pink";
+
+// h1.addEventListener("click", () => {
+//   console.log("Arun Kumar");
+// });
+
+// var inc = document.querySelector("#inc");
+// var dec = document.querySelector("#dec");
+
+// var h2 = document.querySelector("h2");
+
+// var a = 0;
+
+// inc.addEventListener("click", () => {
+//   a++;
+//   h2.innerHTML = a;
+// });
+
+// dec.addEventListener("click", () => {
+//   a--;
+//   h2.innerHTML = a;
+// });
+
+//day 43
+
+// math.random()
+
+// let a = Math.floor(Math.random() * 100);
+// console.log(a);
+
+// var div = document.querySelector("#parent");
+// var btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   // div.style.color = `rbg(${c1},${c2},${c3})`;
+//   // console.log("Hello")
+//   let c1 = Math.floor(Math.random() * 256);
+//   let c2 = Math.floor(Math.random() * 256);
+//   let c3 = Math.floor(Math.random() * 256);
+
+//   div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+//   // console.log(c1,c2,c3)
+// });
+
+// const arr = [
+//   "Arun",
+//   "Rahul",
+//   "Aman",
+//   "Priya",
+//   "Neha",
+//   "Rohit",
+//   "Ankit",
+//   "Pooja",
+//   "Vikas",
+//   "Sneha",
+// ];
+
+// let a = Math.floor(Math.random() * arr.length);
+
+// console.log(arr[a]);
+// array of objects
+
+
