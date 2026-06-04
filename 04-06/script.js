8cd3fdb9600a6c16a2c919e0e758ce054810a831
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-06-04 20:44:28 +0530

    june 4

---
+++ 04-06/script.js
@@ -0,0 +1,56 @@
+// var box = document.querySelector("#box");
+
+// box.addEventListener("mouseenter", () => {
+//   console.log("Enter");
+// });
+
+// box.addEventListener("mouseleave", () => {
+//   console.log("left...");
+// });
+
+// var img = document.querySelector("img");
+// var msg = document.querySelector("h2 span");
+
+// img.addEventListener("mouseenter", () => {
+//   // console.log("Enter");
+//   msg.innerHTML = "door ho ja 🤢";
+// });
+
+// img.addEventListener("mouseleave", () => {
+//   // console.log("left...");
+//   msg.innerHTML = "ab door hi rhna 😢";
+// });
+
+// var main = document.querySelector("#main");
+
+// var cursor = document.querySelector("#cursor");
+
+// main.addEventListener("mousemove", (dets) => {
+//   cursor.style.left = dets.x + "px";
+//   cursor.style.top = dets.y + "px";
+// });
+
+// day 49
+
+// var h1 = document.querySelectorAll('h1');
+
+// h1.forEach(()=>{
+//   console.log("Hello")
+// })
+
+// var arr = [1,2,3,4]
+
+//(.childNodes) ->
+
+// day 50
+
+// allBtn.forEach((elem) => {
+//   elem.addEventListener("click", () => {
+//     console.log("Clicked...");
+//   });
+// });
+
+
+// eventbubbling
+
+
