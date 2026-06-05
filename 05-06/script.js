4d7e96925833d35402f9e7a8ffe6ee773e140d92
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-06-05 11:48:47 +0530

    june 5

---
+++ 05-06/script.js
@@ -0,0 +1,15 @@
+// var arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((elem, idx) => {
+//   console.log(elem, idx);
+// });
+
+// // pointer-events: none;
+
+// function addData() {
+//   console.log("Called...");
+// }
+
+// addData();
+
+
